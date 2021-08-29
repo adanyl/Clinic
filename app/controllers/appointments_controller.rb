919f19c71a47e5b046f0412d1d9723a6a2e5ff58
Oleksii <alexey.danylenko@gmail.com>
2021-08-29 23:46:56 +0300

    added avaliability to make an appointment

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,22 +1,39 @@
 class AppointmentsController < ApplicationController
     def new
-        #@appointment = @doctor.appointments.new
-    end
-  
-    private
-    def set_doctor
-      @doctor = Doctor.find(params[:doctor_id])
+      @appointment = Appointment.new
+      @doctors = Doctor.all
     end
 
-    def set_appointment
+    def create
+      @appointment = Appointment.new(user_params)
+      if @appointment.save
+        redirect_to root_path
+      end
+    end
+
+    def destroy
       @appointment = Appointment.find(params[:id])
+      @content.destroy
+      redirect_to root_path
     end
 
-    def set_users
-      @users = (User.all.order("name") - @doctor.users)
+    def user_params
+      params.require(:appointment).permit(:user_id, :doctor_id, :date, :time)
     end
 
-    def appointment_params
-      params.require(:appointment).permit(:user_id, :date, :time)
+    def show
+      if current_doctor.present?
+        @appointments = Appointment.where(:doctor_id => current_doctor.id)
+      elsif current_user.present?
+        @appointments = Appointment.where(:user_id => current_user.id)
+      end
+    end
+
+    def index
+      if current_doctor.present?
+        @appointments = Appointment.where(:doctor_id => current_doctor.id)
+      elsif current_user.present?
+        @appointments = Appointment.where(:user_id => current_user.id)
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
   
-
+resources :appointments 
+  get '/appointments/terminate'
+  get '/appointments/index'
   get '/doctors/index'
   get '/appointments/new'
 end
