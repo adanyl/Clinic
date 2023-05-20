ce014f806a03f779307acc22d449887afe169c5b
Oleksii Danylenko <alexey.danylenko@gmail.com>
2023-05-20 23:56:52 +0300

    [Fix] edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password',
-                  password_confirmation: 'password')
+AdminUser.create!(email: 'alexey.danylenko@gmail.com', password: 'alex123',
+                  password_confirmation: 'alex123')
